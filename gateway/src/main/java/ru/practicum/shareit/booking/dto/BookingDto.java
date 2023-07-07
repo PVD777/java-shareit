f427fddb0645b79b8c78fbaab56f11f72f7ca7c1
PVD <cheaterpvd@yandex.ru>
2023-07-07 17:55:38 +0300

    fix broken

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -6,12 +6,15 @@ import lombok.experimental.FieldDefaults;
 import ru.practicum.shareit.utility.StartBeforeEndDateValid;
 
 import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 @StartBeforeEndDateValid
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookingDto {
+    @NotNull
+    int itemId;
     @FutureOrPresent
     LocalDateTime start;
     LocalDateTime end;
